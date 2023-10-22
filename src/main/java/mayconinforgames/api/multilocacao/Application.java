4c1dd4a5dab541a4ecbf0d68027d21cfdea5bc0e
MayconInforGames2017 <maycon_gus2009@hotmail.com>
2023-10-22 19:54:29 -0300

    Criando projeto

---
+++ src/main/java/mayconinforgames/api/multilocacao/Application.java
@@ -0,0 +1,13 @@
+package mayconinforgames.api.multilocacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/mayconinforgames/api/multilocacao/ApplicationTests.java
@@ -0,0 +1,13 @@
+package mayconinforgames.api.multilocacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
